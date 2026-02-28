6fbddc5ad753389566f4d131052de884f9b61044
Nithin Das <nithindaskavungal@gmail.com>
2026-02-28 19:20:51 +0530

    fix(ui): change product list to single column on mobile

--- frontend/src/pages/user/InventoryPage.tsx
+++ frontend/src/pages/user/InventoryPage.tsx
@@ -237,7 +237,7 @@ const InventoryPage: React.FC = () => {
                     </AlertDialog>
 
                     {/* Mobile & Tablet Cards */}
-                    <div className="grid grid-cols-2 lg:grid-cols-3 gap-3 sm:gap-4 xl:hidden">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 sm:gap-4 xl:hidden">
                         {loading ? (
                             <div className="col-span-full py-12 flex justify-center">
                                 <Spinner className="mx-auto" />
