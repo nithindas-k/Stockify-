3951ced6a7e4f010c3d8b7ffcdcfaf56bfe0691d
Nithin Das <nithindaskavungal@gmail.com>
2026-03-06 17:52:29 +0530

    Fix(smtp): fixed

--- backend/src/utils/MailService.ts
+++ backend/src/utils/MailService.ts
@@ -8,11 +8,16 @@ export class MailService {
 
   constructor() {
     this.transporter = nodemailer.createTransport({
-      service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
       },
+      tls: {
+        rejectUnauthorized: false
+      }
     });
   }
 
